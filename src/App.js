50c502e0d6a15abbadbba0e66bf98701b605dcfd
emaden99 <emaden99@gmail.com>
2021-05-25 00:44:54 +0300

    mainpage
    main page version1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import { Home } from "./containers/Home";
+import  {Home} from "./containers/Home";
 import { SignIn } from "./containers/SignIn";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-
+import Header from "./components/Header";
 import { Container, Row, Col } from "reactstrap";
 
 function App() {
-  return (
-    <Container>
-      <Router>
-        <Switch>
-          <Route path="/signin">
-            <SignIn />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </Router>
-    </Container>
+  return ( 
+    <div>
+    <Header/>
+  <Home/>
+
+  </div>
   );
 }
 
